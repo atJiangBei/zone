18b23ca828b50f856019ae1840df05670e9a0c70
jiangbei <engineersweb@163.com>
2019-03-18 21:27:48 +0800

    测试数据库

--- controller/outerapi.js
+++ controller/outerapi.js
@@ -1,3 +1,7 @@
+const User = require("./../datamodel");
+
+
+
 exports.signin = async (ctx,next)=>{
     ctx.body = '登录';
     
@@ -6,6 +10,10 @@ exports.signout = async (ctx,name)=>{
 	ctx.body = '退出登录'
 }
 exports.register = async (ctx,name)=>{
+	const user = new User({name:"狗狗"});
+	user.save((err,user)=>{
+		if(err) return console.log(err);
+	})
 	ctx.body = {
         title: '注册'
       }
